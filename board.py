e598bdade68bb9517fa2c053e69e0d7afd50aaf9
Boštjan Mejak <bostjan.xperia@gmail.com>
2017-11-30 23:12:09 +0100

    Add new modules
    I have started this application from the ground up.

---
+++ board.py
@@ -0,0 +1,41 @@
+"""
+Docstring.
+"""
+import chess
+import chess.svg
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtWidgets import QWidget
+
+
+class Chessboard(QWidget):
+    """
+    Docstring.
+    """
+    def __init__(self):
+        """
+        Docstring.
+        """
+        super().__init__()
+
+        self.chessboard = chess.Board()
+        self.drawChessboard()
+
+    @pyqtSlot(QSvgWidget)
+    def mouseReleaseEvent(self, event):
+        """
+        Docstring.
+        """
+        print("Mouse key released!")
+
+    def drawChessboard(self):
+        """
+        Docstring.
+        """
+        self.svgChessboard = chess.svg.board(board=self.chessboard)
+        self.svgChessboardEncoded = self.svgChessboard.encode("utf-8")
+        self.svgWidget = QSvgWidget(parent=self)
+        self.svgWidget.setGeometry(10, 10, 400, 400)
+
+        return self.svgWidget.load(self.svgChessboardEncoded)
